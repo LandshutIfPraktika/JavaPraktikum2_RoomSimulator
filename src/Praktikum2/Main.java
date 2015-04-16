082e15817f41359dd9888ee6c9e8a5c598c69cdd
s-gheldd <s-gheldd@haw-landshut.de>
2015-04-16 17:49:21 +0200

    toString in Manufacturer

--- src/Praktikum2/Main.java
+++ src/Praktikum2/Main.java
@@ -10,5 +10,6 @@ public class Main {
         myRoom.getLocker().open();
         myRoom.print();
 
+
     }
 }
--- src/Praktikum2/Manufacturer.java
+++ src/Praktikum2/Manufacturer.java
@@ -4,5 +4,21 @@ package Praktikum2;
  * Created by Georg on 15.04.2015.
  * U+00A9 all tights reserved
  */
-public enum Manufacturer { IKEA, XXXLUTZ, ROLFBENZ, UNKNOWN
+public enum Manufacturer { IKEA, XXXLUTZ, ROLFBENZ, UNKNOWN;
+
+
+    public String toString(){
+        switch(this){
+
+            case IKEA:
+                return "Ikea";
+            case XXXLUTZ:
+                return "XXXLutz";
+            case ROLFBENZ:
+                return "Rolf-Benz";
+            case UNKNOWN:
+                return "unknown/self-made";
+        }
+        return "";
+    }
 }
